3e52e2be082f2ebb0686f954e1e037ef433c6541
stefan.najdovski <stefan.najdovski@aol.com>
2025-08-15 17:58:53 +0200

    debugging with zed

--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main([[maybe_unused]]int argc, [[maybe_unused]]char* argv[]){
 
-    write(1,"Hello, World!\n",15);
+    char *header = "HTTP/1.1 200 OK";
+    write(1,header,strlen(header));
 
 
     return EXIT_SUCCESS;
