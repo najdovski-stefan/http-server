e1164c1740b783e3069377a71436277f7aebbb74
stefan.najdovski <stefan.najdovski@aol.com>
2025-08-26 16:05:07 +0200

    style: fix indentation inconsistencies

--- src/main.c
+++ src/main.c
@@ -8,48 +8,48 @@
 
 
 char *parse_get(char *get_request, size_t req_length){
-    char *start,   *end;
+
+    char *start, *end;
 
     //man ascii 32? 40? ' ' -> 32(int) 20 hex
 
     for(start = get_request; start[0] != ' '; start++){
-
         if (start[0] == '\0') {
             return NULL;
         }
     }
 
-        start++;
+    start++;
 
-        for(end = start; start[0] != ' '; end++){
-            if(start[0] == '\0'){
-                return NULL;
-            }
-        }
-
-        if(end[-1] != '/'){
-            end[0] = '/';
-            end++;
-        }
-
-        //malformed GET Request
-        if(end + strlen(INDEX_FILE) > get_request + req_length){
+    for(end = start; start[0] != ' '; end++){
+        if(start[0] == '\0'){
             return NULL;
         }
-
-        memcpy(end,INDEX_FILE,strlen(INDEX_FILE)+1);
-
-       return start+1;
     }
+
+    if(end[-1] != '/'){
+        end[0] = '/';
+        end++;
+    }
+
+    //malformed GET Request
+    if(end + strlen(INDEX_FILE) > get_request + req_length){
+        return NULL;
+    }
+
+    memcpy(end,INDEX_FILE,strlen(INDEX_FILE)+1);
+
+    return start+1;
+}
 
 int main([[maybe_unused]]int argc, [[maybe_unused]]char* argv[]){
 
     char *header = "HTTP/1.1 200 OK";
     write(1,header,strlen(header));
 
-    char request[] = "GET / HTTP/1.1\nHost:example.com";
+    char request[] = "GET / HTTP/1.1\nHost: example.com";
 
-    //printf("%s\"\n", parse_get(request, strlen(request)));
+    printf("%s\"\n", parse_get(request, strlen(request)));
 
 
     return EXIT_SUCCESS;
